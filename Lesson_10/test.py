b1c5fb5f5e0b86a587635841701e3ebb49ab90c9
yurycash <yury.cash@yandex.ru>
2024-09-17 11:45:56 +0300

    Allure_11

--- Lesson_10/test.py
+++ Lesson_10/test.py
@@ -4,7 +4,8 @@ from API import Company
 from data_base import DbEmployee
 
 db = DbEmployee(
-    "postgresql://x_clients_user:95PM5lQE0NfzJWDQmLjbZ45ewrz1fLYa@dpg-cqsr9ulumphs73c2q40g-a.frankfurt-postgres.render.com/x_clients_db_fxd0")
+    "postgresql://x_clients_user:95PM5lQE0NfzJWDQmLjbZ45ewrz1fLYa@dpg-cqsr9ulumphs73c2q40g-a.frankfurt-postgres.render.com/x_clients_db_fxd0"
+    )
 
 company = Company("https://x-clients-be.onrender.com")
 
@@ -47,11 +48,14 @@ def test_get_list_employee2():
 @allure.feature("POST")
 @allure.severity("Critical")
 def test_add_employee2():
-    db_result = db.get_list_employee(company_id)
-    api.add_new_employee2(body)
-    api_response = api.get_list_employee2(param_id)
-    api_response = api_response.json()
-    assert len(api_response)-len(db_result) == 1
+    with allure.step("Получить сотрудников через БД"):
+        db_result = db.get_list_employee(company_id)
+    with allure.step("Создание сотрудника через API"):
+        api.add_new_employee2(body)
+        api_response = api.get_list_employee2(param_id)
+        api_response = api_response.json()
+    with allure.step("Проверка что список увичился на 1"):
+        assert len(api_response)-len(db_result) == 1
 
 
 @allure.title("Получение данных сотрудника по ID.")
@@ -59,10 +63,13 @@ def test_add_employee2():
 @allure.feature("GET")
 @allure.severity("Major")
 def test_get_new_employee2():
-    resp = api.get_list_employee2(param_id)
-    api_new_employee = resp.json()[-1]['id']
-    db_new_employee = db.get_id_new_employee()
-    assert api_new_employee == db_new_employee
+    with allure.step("Получение ID сотрудника через API"):
+        resp = api.get_list_employee2(param_id)
+        api_new_employee = resp.json()[-1]['id']
+    with allure.step("Получение ID сотрудника через БД"):
+        db_new_employee = db.get_id_new_employee()
+    with allure.step("Сравнение ID"):
+        assert api_new_employee == db_new_employee
 
 
 @allure.title("Добавление нового сотрудника.")
@@ -70,15 +77,17 @@ def test_get_new_employee2():
 @allure.feature("POST")
 @allure.severity("Blocker")
 def test_create_employee():
-    db_result = db.get_list_employee(company_id)
-    db.add_new_employee("Юрий", "Горбатенко", "891294443688", True, company_id)
-    data_employee = api.get_new_employee2(db.get_id_new_employee())
-    data_employee = data_employee.json()
-    assert data_employee["firstName"] == "Юрий"
-    assert data_employee["lastName"] == "Горбатенко"
-    assert data_employee["phone"] == "891294443688"
-    assert data_employee["isActive"] == True
-    assert data_employee["companyId"] == company_id
+    with allure.step("Создание сотрудника через БД"):
+        db.add_new_employee("Юрий", "Горбатенко", "891294443688", True,
+                            company_id)
+        data_employee = api.get_new_employee2(db.get_id_new_employee())
+        data_employee = data_employee.json()
+    with allure.step("Проверка данных сотрудника"):
+        assert data_employee["firstName"] == "Юрий"
+        assert data_employee["lastName"] == "Горбатенко"
+        assert data_employee["phone"] == "891294443688"
+        assert data_employee["isActive"] == True
+        assert data_employee["companyId"] == company_id
     id = db.get_id_new_employee()
     db.delete(id)
 
@@ -88,16 +97,19 @@ def test_create_employee():
 @allure.feature("PATCH")
 @allure.severity("Blocker")
 def test_edit_employee():
-    db.add_new_employee("Юрий", "Горбатенко", "891294443688", True, company_id)
-    id = db.get_id_new_employee()
-    db.edit_employee("Иван", "Петров", "0987654321", True, company_id, id)
+    with allure.step("Содание сотрудника"):
+        db.add_new_employee("Юрий", "Горбатенко", "891294443688", True,
+                            company_id)
+        id = db.get_id_new_employee()
+    with allure.step("Изменение данных сотрудника"):
+        db.edit_employee("Иван", "Петров", "0987654321", True, company_id, id)
     data_employee = api.get_new_employee2(id)
     data_employee = data_employee.json()
-    assert data_employee["firstName"] == "Иван"
-    assert data_employee["lastName"] == "Петров"
-    assert data_employee["phone"] == "0987654321"
-    assert data_employee["isActive"] == True
-    assert data_employee["companyId"] == company_id
+    with allure.step("Проверка измененных данных сотрудника"):
+        assert data_employee["firstName"] == "Иван"
+        assert data_employee["lastName"] == "Петров"
+        assert data_employee["phone"] == "0987654321"
+        assert data_employee["isActive"] == True
+        assert data_employee["companyId"] == company_id
     id = db.get_id_new_employee()
     db.delete(id)
-
